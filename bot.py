5d1ef7706d19ca95c48121443dc349b041afe9f1
GuruPrasaath Manirajan <56358942+GuruPrasaathM@users.noreply.github.com>
2020-07-19 21:51:31 +0530

    reversed back
    will be trying again

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import discord
 import json
 import random
-from discord.ext import commands, tasks, Activity, ActivityType
-from itertools import cycle
+from discord.ext import commands, tasks
 import os
 
 bot = commands.Bot(command_prefix='.')
@@ -14,11 +13,6 @@ status = cycle(['Just Chilling', 'Eating Children', 'Sleeping', 'Bombing Orphana
 async def on_ready():
     change_status.start()
     print('Bot is online.')
-
-@bot.event
-async def howmany():
-    gcount=str(len(bot.guilds))
-    await ctx.send(f'Currently active in {gcount} servers')
 
 @bot.event
 async def on_command_error(ctx, error):
